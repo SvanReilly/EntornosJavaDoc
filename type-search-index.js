c2d41e0f1bec77b3e64b89bf0e9a92237ee81d34
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2023-03-25 16:12:29 +0100

    FinalcommitforGitHubPages

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"JAVADOC","l":"CELDA"},{"p":"JAVADOC","l":"GAME"},{"p":"JAVADOC","l":"JUGADOR"},{"p":"JAVADOC","l":"TABLERO"},{"p":"JAVADOC","l":"TresEnRaya"}];updateSearchResults();
